e16974a17975f60b4108f57f595005fa0e7203d6
Dave Pullig <dave@deliciousmedia.co.uk>
2017-10-31 11:06:47 +0000

    Remove unused/unwanted stuff.

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -18,10 +18,12 @@
 		<?php
 			the_content();
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
-				'after'  => '</div>',
-			) );
+			wp_link_pages(
+				[
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
+					'after'  => '</div>',
+				]
+			);
 		?>
 	</div><!-- .entry-content -->
 
@@ -33,11 +35,11 @@
 						wp_kses(
 							/* translators: %s: Name of current post. Only visible to screen readers */
 							__( 'Edit <span class="screen-reader-text">%s</span>', '_s' ),
-							array(
-								'span' => array(
-									'class' => array(),
-								),
-							)
+							[
+								'span' => [
+									'class' => [],
+								],
+							]
 						),
 						get_the_title()
 					),
