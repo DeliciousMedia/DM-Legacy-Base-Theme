1e476615882c968063c238c4c04ac56ed8c73296
Dave Pullig <dave@deliciousmedia.co.uk>
2017-11-01 14:33:15 +0000

    Initial gulp implementation

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+require('es6-promise').polyfill();
+
+var gulp          = require('gulp'),
+    sass          = require('gulp-sass'),
+    sourcemaps    = require('gulp-sourcemaps');
+    //rtlcss        = require('gulp-rtlcss'),
+    autoprefixer  = require('gulp-autoprefixer'),
+    plumber       = require('gulp-plumber'),
+    gutil         = require('gulp-util'),
+    rename        = require('gulp-rename'),
+    concat        = require('gulp-concat'),
+    jshint        = require('gulp-jshint'),
+    uglify        = require('gulp-uglify'),
+    imagemin      = require('gulp-imagemin'),
+    cssnano       = require('gulp-cssnano'),
+    cached        = require('gulp-cached');
+
+var onError = function( err ) {
+  console.log('An error occurred:', gutil.colors.magenta(err.message));
+  gutil.beep();
+  this.emit('end');
+};
+
+// Sass
+gulp.task('sass', function() {
+  return gulp.src('./src/sass/**/*.scss')
+  .pipe(plumber({ errorHandler: onError }))
+  .pipe(sourcemaps.init())
+  .pipe(sass())
+  .pipe(autoprefixer())
+  .pipe(cssnano())
+  .pipe(sourcemaps.write('.'))
+  .pipe(gulp.dest('./assets/css'));
+//  .pipe(rtlcss())                     // Convert to RTL
+//  .pipe(rename({ basename: 'rtl' }))  // Rename to rtl.css
+//  .pipe(gulp.dest('./'));             // Output RTL stylesheets (rtl.css)
+});
+
+// JavaScript
+gulp.task('js', function() {
+  return gulp.src(['./src/js/*.js'])
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'))
+  //.pipe(concat('app.js'))
+  .pipe(rename({suffix: '.min'}))
+  .pipe(uglify())
+  .pipe(gulp.dest('./assets/js'));
+});
+
+// Images
+gulp.task('images', function() {
+  return gulp.src('./src/img/*')
+  .pipe(plumber({ errorHandler: onError }))
+  .pipe(cached('images'))
+  .pipe(imagemin({ progressive: true }))
+  .pipe(gulp.dest('./assets/img'));
+});
+
+// Watch
+gulp.task('watch', function() {
+  gulp.watch('src/sass/**/*.scss', ['sass']);
+  gulp.watch('src/js/*.js', ['js']);
+  gulp.watch('src/img/**/*', ['images']);
+});
+
+gulp.task('default', ['sass', 'js', 'images', 'watch']);
