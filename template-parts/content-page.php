15a4078a4765f98eeaf18ada021ea8edb878ee1c
Dave Pullig <dave@deliciousmedia.co.uk>
2017-11-08 16:47:15 +0000

    Remove post edit links

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -27,26 +27,6 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<?php if ( get_edit_post_link() ) : ?>
 		<footer class="entry-footer">
-			<?php
-				edit_post_link(
-					sprintf(
-						wp_kses(
-							/* translators: %s: Name of current post. Only visible to screen readers */
-							__( 'Edit <span class="screen-reader-text">%s</span>', '_s' ),
-							[
-								'span' => [
-									'class' => [],
-								],
-							]
-						),
-						get_the_title()
-					),
-					'<span class="edit-link">',
-					'</span>'
-				);
-			?>
 		</footer><!-- .entry-footer -->
-	<?php endif; ?>
 </article><!-- #post-<?php the_ID(); ?> -->
